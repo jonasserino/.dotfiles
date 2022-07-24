b11fd74b0435abc1e6b774bafe8856653e4142eb
jonasserino <jonasserino@gmail.com>
2022-07-24 11:57:20 +0800

    added telescope

---
+++ nvim/.config/nvim/lua/jonasserino/packer.lua
@@ -0,0 +1,7 @@
+return require("packer").startup(function()
+	use("wbthomason/packer.nvim")
+
+	use("nvim-lua/plenary.nvim")
+	use("nvim-lua/popup.nvim")
+	use("nvim-telescope/telescope.nvim")
+end)
