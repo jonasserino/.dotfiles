ab91e09b698e27e3cdbfd23e8c35f827afa9a674
jonasserino <jonasserino@gmail.com>
2022-07-24 12:38:00 +0800

    added nvim-cmp

--- nvim/.config/nvim/lua/jonasserino/init.lua
+++ nvim/.config/nvim/lua/jonasserino/init.lua
@@ -1,6 +1,7 @@
 require("jonasserino.set")
 require("jonasserino.keymap")
 require("jonasserino.packer")
+require("jonasserino.cmp")
 
 vim.g.netrw_keepdir = 0
 vim.g.netrw_banner = 0
--- nvim/.config/nvim/lua/jonasserino/packer.lua
+++ nvim/.config/nvim/lua/jonasserino/packer.lua
@@ -4,4 +4,12 @@ return require("packer").startup(function()
 	use("nvim-lua/plenary.nvim")
 	use("nvim-lua/popup.nvim")
 	use("nvim-telescope/telescope.nvim")
+
+    use("hrsh7th/nvim-cmp")
+    use("hrsh7th/cmp-buffer")
+    use("hrsh7th/cmp-path")
+    use("hrsh7th/cmp-cmdline")
+    use("saadparwaiz1/cmp_luasnip")
+
+    use("L3MON4D3/LuaSnip")
 end)
