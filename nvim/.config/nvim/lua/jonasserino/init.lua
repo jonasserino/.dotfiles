458c1da17dc0a518bfd3e8d1e99a7242c742f47b
jonasserino <jonasserino@gmail.com>
2022-07-24 11:58:12 +0800

    added and changed bindings to telescope and netrw explorer respectively

--- nvim/.config/nvim/lua/jonasserino/init.lua
+++ nvim/.config/nvim/lua/jonasserino/init.lua
@@ -1,2 +1,6 @@
 require("jonasserino.set")
 require("jonasserino.keymap")
+require("jonasserino.packer")
+
+vim.g.netrw_keepdir = 0
+vim.g.netrw_banner = 0
--- nvim/.config/nvim/lua/jonasserino/set.lua
+++ nvim/.config/nvim/lua/jonasserino/set.lua
@@ -4,5 +4,10 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
 vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
 
 vim.opt.scrolloff = 8
+
+vim.opt.updatetime = 50
+
+vim.g.mapleader = " "
