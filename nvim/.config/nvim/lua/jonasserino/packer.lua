47ac1d13e3b5e9ac9aac61ccb5131ee7976bcf07
jonasserino <jonasserino@gmail.com>
2022-07-27 19:45:44 +0800

    added telescope to nvim

--- nvim/.config/nvim/lua/jonasserino/packer.lua
+++ nvim/.config/nvim/lua/jonasserino/packer.lua
@@ -6,6 +6,10 @@ vim.cmd [[packadd packer.nvim]]
 return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
+
+  use 'nvim-lua/plenary.nvim'
+  use 'nvim-telescope/telescope.nvim'
+
   use 'neovim/nvim-lspconfig'
 
   use("nvim-treesitter/nvim-treesitter", { run = ':TSUpdate' })
--- nvim/.config/nvim/lua/jonasserino/remap.lua
+++ nvim/.config/nvim/lua/jonasserino/remap.lua
@@ -1,3 +1,8 @@
 local nnoremap = require("jonasserino.keymap").nnoremap
 
 nnoremap("<leader>pv", "<cmd>Ex<CR>")
+
+nnoremap("<leader>ff", "<cmd>Telescope find_files<CR>")
+nnoremap("<leader>fg", "<cmd>Telescope live_grep<CR>")
+nnoremap("<leader>fb", "<cmd>Telescope buffers<CR>")
+nnoremap("<leader>fh", "<cmd>Telescope help_tags<CR>")
