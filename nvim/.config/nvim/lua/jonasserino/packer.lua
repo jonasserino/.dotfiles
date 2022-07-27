ca72900383e30b36a22cbb2506784316fdf3a233
jonasserino <jonasserino@gmail.com>
2022-07-27 18:02:43 +0800

    added treesitter to nvim

--- nvim/.config/nvim/lua/jonasserino/packer.lua
+++ nvim/.config/nvim/lua/jonasserino/packer.lua
@@ -7,4 +7,6 @@ return require('packer').startup(function(use)
   -- Packer can manage itself
   use 'wbthomason/packer.nvim'
   use 'neovim/nvim-lspconfig'
+
+  use("nvim-treesitter/nvim-treesitter", { run = ':TSUpdate' })
 end)
